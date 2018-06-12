18cc78b8be8c7874a1ab78960a196b962026384d
simplepeng <383559698@qq.com>
2018-06-12 21:12:57 +0800

    mine layout

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -1,18 +1,25 @@
 // pages/mine/mine.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+   userInfo:{},
+   hasUserInfo:false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-  
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
   },
 
   /**
