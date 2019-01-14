0b3b7c3d5f74ed451c634d11b21bbed1fe1f0859
陈鹏 <383559698@qq.com>
2019-01-14 17:22:23 +0800

    add:cloud function

--- app.js
+++ app.js
@@ -1,30 +1,62 @@
+let that;
+
 App({
+
+  auth: false,
 
   /**
    * 当小程序初始化完成时，会触发 onLaunch（全局只触发一次）
    */
-  onLaunch: function () {
-    
+  onLaunch: function() {
+    that = this;
+    wx.cloud.init()
   },
 
   /**
    * 当小程序启动，或从后台进入前台显示，会触发 onShow
    */
-  onShow: function (options) {
-    
+  onShow: function(options) {
+
   },
 
   /**
    * 当小程序从前台进入后台，会触发 onHide
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 当小程序发生脚本错误，或者 api 调用失败时，会触发 onError 并带上错误信息
    */
-  onError: function (msg) {
-    
+  onError: function(msg) {
+
   }
 })
+
+function getIds(){
+  wx.cloud.callFunction({
+    // 云函数名称
+    name: 'index',
+    // 传给云函数的参数
+    data: {
+      
+    },
+  })
+    .then(res => {
+      console.log(res.result) // 3
+    })
+    .catch(console.error)
+}
+
+function getAuth() {
+  wx.getSetting({
+    success(res) {
+      console.log(res);
+      that.auth = true;
+    },
+    fail(e){
+      
+    }
+  })
+}
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,67 +1,76 @@
+const app = getApp();
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    userInfo:false,
-    isInput:true
+    userInfo: null,
+    isInput: false,
+    isAuth: app.auth
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     
+    wx.cloud.callFunction({
+      name:'index'
+    }).then(res => {
+      console.log(res.result) // 3
+    }).catch(console.error);
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   },
   // 获取用户信息
   onGotUserInfo(e) {
@@ -69,6 +78,43 @@ Page({
     // console.log(e.detail.errMsg)
     console.log(e.detail.userInfo)
     // console.log(e.detail.rawData)
-    
+    this.setData({
+      userInfo: e.detail.userInfo
+    })
   },
+  /**
+   * 
+   */
+  addTask() {
+    console.log("添加新的任务");
+    this.setData({
+      isInput: true
+    })
+  },
+  /**
+   * 
+   */
+  submitTask(e) {
+    console.log("提交新的任务");
+    console.log(e.detail.value.textarea)
+
+  }
 })
+
+function wxLogin() {
+  wx.login({
+    success(res) {
+      if (res.code) {
+        // 发起网络请求
+        wx.request({
+          url: 'https://test.com/onLogin',
+          data: {
+            code: res.code
+          }
+        })
+      } else {
+        console.log('登录失败！' + res.errMsg)
+      }
+    }
+  })
+}
